81f98b7791002131e315d3a8fff0f98088d8389c
Sumanth-Talluri <tallurisaisumanth77@gmail.com>
2023-11-25 22:28:52 -0700

    Added timestamp to constructor

--- src/contracts/VotingContract.sol
+++ src/contracts/VotingContract.sol
@@ -22,10 +22,6 @@ contract VotingContract {
     );
 
     constructor () public {
-        addCandidate("Sumanth");
-        addCandidate("Ravi Kanth");
-        addCandidate("Praveen");
-
         // Initialize unique voter IDs
         uniqueVoterIds[123] = true;
         uniqueVoterIds[456] = true;
@@ -36,13 +32,14 @@ contract VotingContract {
         adminAccountIds[address(0xAe1DdF39a90FeeAc8708739aacCa6e8781daC6c6)] = true;
         adminAccountIds[address(0x4b3527ad07fA1Ab3E7bFe83ec18cC6cB57d6c908)] = true;
         adminAccountIds[address(0x76D81132eb074d4d2277fB10FdF14177fBFA7341)] = true;
+
+        votingEndTime = block.timestamp;
     }
 
     function addCandidate(string memory _name) public {
         require(adminAccountIds[msg.sender], "Only admin accounts can add the candidate"); // Check if msg.sender is an admin
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
-        votingEndTime = block.timestamp;
     }
 
     function vote (uint _candidateId) public {
@@ -53,7 +50,7 @@ contract VotingContract {
         emit votedEvent(_candidateId);
     }
 
-    function getCandidates() public view returns (Candidate[] memory) {
+    function getCandidates() public returns (Candidate[] memory) {
         Candidate[] memory result = new Candidate[](candidatesCount);
 
         for (uint i = 1; i <= candidatesCount; i++) {
@@ -63,7 +60,7 @@ contract VotingContract {
         return result;
     }
 
-    function getVotingEndTime() public view returns (uint) {
+    function getVotingEndTime() public returns (uint) {
         return votingEndTime;
     }
 
@@ -72,12 +69,12 @@ contract VotingContract {
         votingEndTime = newTimeStamp;
     }
 
-    function checkIfVoterIdExists(uint voterId) public view returns (bool) {
+    function checkIfVoterIdExists(uint voterId) public returns (bool) {
         return uniqueVoterIds[voterId];
     }
 
     // Updated function to check if an admin account exists
-    function checkIfAdminUser(address adminAccountId) public view returns (bool) {
+    function checkIfAdminUser(address adminAccountId) public returns (bool) {
         return adminAccountIds[adminAccountId];
     }
 }
